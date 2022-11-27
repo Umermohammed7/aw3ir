da34a5702cd789af185da4a5294701fc9fda1202
Taiserwar <115471790+Taiserwar@users.noreply.github.com>
2022-11-27 10:27:54 +0100

    32

--- tp4/js/form-validation.js
+++ tp4/js/form-validation.js
@@ -76,13 +76,19 @@ var contactStore = (function () {
         };
         // ajout du contact à la liste
         contactList.push(contact);
-  
+        
         return contactList;
       },
   
       getList: function () {
         return contactList;
       },
+
+      showList: function(){
+        for (var index in contactList) {
+          console.log(contactList[index].name);
+        }
+      }
     };
   })();
 
@@ -114,12 +120,23 @@ function validateEmail(mail) {
 
   function validate() {
 
+
+
     var myModal = new bootstrap.Modal(document.getElementById("myModal"));
     let n = document.getElementById("nom").value;
     let p = document.getElementById("prénom").value;
     let a = document.getElementById("adresse").value;
     let e = document.getElementById("mail").value;
     let da = document.getElementById("date").value;
+
+    document.querySelector("table tbody").innerHTML =
+    "<tr>" +
+    "<th> Nom</th>" +
+    "<th> Prénom</th>" +
+    "<th> Date de naissance</th>" +
+    "<th> Adresse</th>" +
+    "<th> Mail</th>" +
+    "</tr>" ;
 
     if (n.length < 5 || p.length < 5 || a.length < 5 || validateEmail(e) == false || testDate(da) == false) {
 
@@ -128,19 +145,34 @@ function validateEmail(mail) {
         myModal.show();
 
     } else {
+      let _nom = document.getElementById("nom").value;
+      let _prénom = document.getElementById("prénom").value;
+      let _adresse = document.getElementById("adresse").value;
+      let _mail = document.getElementById("mail").value;
+      let _date = document.getElementById("date").value;
 
         contactStore.add(_nom, _prénom, _date, _adresse, _mail);
-        for (var index in contactList) {
+       /* for (var index in contactList) {
             console.log(contactList[index].nom);
-          }
+          }*/
+          contactStore.showList();
           document.querySelector("table tbody").innerHTML =
           document.querySelector("table tbody").innerHTML +
           "<tr><td>" +
-          nom +
+          n +
           "</td><td>" +
-          prénom +
-          "</td><td>";
-        // CODE à compléter pour insérer les autres données
+          p +
+          "</td><td>" +
+           
+          da +
+          "</td><td>" +
+          a +
+          "</td><td>" +
+          
+          e +
+          "</td></tr>";
+
+        // CODE à compléter pour insérer les autres données 
     }
 
 
